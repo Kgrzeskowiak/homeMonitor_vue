33206fd477f15347cdc9d6efa9ceefe8f70387a0
Kgrzeskowiak <Kgrzeskowiak@users.noreply.github.com>
2019-05-19 09:35:11 +0200

    Update Charts.vue

--- src/views/Charts.vue
+++ src/views/Charts.vue
@@ -50,36 +50,54 @@ export default {
       this.loaded= true;
        })
     },
-    renderChartForSensor(sensorId)
-    {
-      this.loaded = false; //Tylko dzieki temu sie przebudowuje
-       this.queryParams = {sensorName : sensorId, timeRange :-1}
-       this.eraseChart()
-       this.$store.dispatch("getReadingsForSensorInTime", this.queryParams).then(response => {
-       this.$store.state.readings.forEach(element => {
-       this.dataForChart.labels.push(element.measurmentDate)
-       this.dataForChart.datasets[0].data.push(element.temperature)
-       this.dataForChart.datasets[1].data.push(element.humidity)
-         })
-      this.loaded= true;
-       })
+   renderChartForSensor(sensorId) {
+      let model = {
+        labels: [],
+        datasets: [
+          {
+            label: "Temperatura",
+            backgroundColor: "#f44242",
+            data: []
+          },
+          {
+            label: "Wilgotność",
+            backgroundColor: "#41bbf4",
+            data: []
+          }
+        ]
+      };
+
+      this.queryParams = { sensorName: sensorId, timeRange: -1 };
+      this.eraseChart();
+      this.$store
+        .dispatch("getReadingsForSensorInTime", this.queryParams)
+        .then(response => {
+          this.$store.state.readings.forEach(element => {
+            model.labels.push(element.measurmentDate);
+            model.datasets[0].data.push(element.temperature);
+            model.datasets[1].data.push(element.humidity);
+          });
+          this.dataForChart = model;
+        });
     },
-    eraseChart()
-    {
-       while (this.dataForChart.labels.length > 0)
-       {
-         this.dataForChart.labels.pop();
-       }
-       while (this.dataForChart.datasets[0].data.length > 0)
-       {
-         this.dataForChart.datasets[0].data.pop()
-       }
-       while (this.dataForChart.datasets[1].data.length > 0)
-       {
-         this.dataForChart.datasets[1].data.pop()
-       }
+ eraseChart() {
+      this.dataForChart = {
+        labels: [],
+        datasets: [
+          {
+            label: "Temperatura",
+            backgroundColor: "#f44242",
+            data: []
+          },
+          {
+            label: "Wilgotność",
+            backgroundColor: "#41bbf4",
+            data: []
+          }
+        ]
+      };
     }
-    
+
   }
 }
 </script>
